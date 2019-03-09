fa2272ea581d14de7a7c18ae463645ff821e5051
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2019-03-08 23:28:42 -0600

    Mobile & Table

--- src/Components/Contacto/Contacto.js
+++ src/Components/Contacto/Contacto.js
@@ -6,17 +6,17 @@ export default class Contacto extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className='cantainerCon'>
+                <div className='cantainerCon contTerm'>
                     <div className="fakeMenu">
                         <div className="fakeButtons fakeClose"></div>
                         <div className="fakeButtons fakeMinimize"></div>
                         <div className="fakeButtons fakeZoom"></div>
                     </div>
                     <div className="fakeScreen">
-                        <p className="line1">&#91;&nbsp;&ldquo;Si te apetece un chat&rdquo;,<span className="cursor1">_</span></p>
-                        <p className="line2">&nbsp;&nbsp;&ldquo;no dudes en contactarme&rdquo;,<span className="cursor2">_</span></p>
-                        <p className="line3">&nbsp;&nbsp;&ldquo;su.mulato.ramirez@gmail.com&rdquo;&nbsp;&#93;<span className="cursor3">_</span></p>
-                        <p className="line4">><span className="cursor4">_</span></p>
+                        <p className="line1 textleft1">&#91;&nbsp;&ldquo;Si te apetece un chat&rdquo;,<span className="cursor1">_</span></p>
+                        <p className="line2 textleft1">&nbsp;&nbsp;&ldquo;no dudes en contactarme&rdquo;,<span className="cursor2">_</span></p>
+                        <p className="line3 textleft1">&nbsp;&nbsp;&ldquo;su.mulato.ramirez@gmail.com&rdquo;&nbsp;&#93;<span className="cursor3">_</span></p>
+                        <p className="line4 textleft1">><span className="cursor4">_</span></p>
                     </div>
                 </div>
             </React.Fragment>
--- src/Components/Terminal/Terminal.js
+++ src/Components/Terminal/Terminal.js
@@ -11,17 +11,17 @@ export default class Terminal extends Component {
 
         return (
             <React.Fragment>
-                <div className=''>
+                <div className='contTerm'>
                     <div className="fakeMenu">
                         <div className="fakeButtons fakeClose"></div>
                         <div className="fakeButtons fakeMinimize"></div>
                         <div className="fakeButtons fakeZoom"></div>
                     </div>
                     <div className="fakeScreen">
-                        <p className="line1">&#91;&nbsp;&ldquo;Hola mi nombre es &rdquo;,<span className="cursor1">_</span></p>
-                        <p className="line2">&nbsp;&nbsp;&ldquo;Suzette Mulato Ramírez&rdquo;,<span className="cursor2">_</span></p>
-                        <p className="line3">&nbsp;&nbsp;&ldquo;y soy Desarrollador web.&rdquo;&nbsp;&#93;<span className="cursor3">_</span></p>
-                        <p className="line4">><span className="cursor4">_</span></p>
+                        <p className="line1 textleft1">&#91;&nbsp;&ldquo;Hola mi nombre es &rdquo;,<span className="cursor1">_</span></p>
+                        <p className="line2 textleft1">&nbsp;&nbsp;&ldquo;Suzette Mulato Ramírez&rdquo;,<span className="cursor2">_</span></p>
+                        <p className="line3 textleft1">&nbsp;&nbsp;&ldquo;y soy Desarrollador web.&rdquo;&nbsp;&#93;<span className="cursor3">_</span></p>
+                        <p className="line4 textleft1">><span className="cursor4">_</span></p>
                     </div>
                 </div>
             </React.Fragment>
